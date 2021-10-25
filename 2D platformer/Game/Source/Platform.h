1a7da33f35b1ab0de5ab78413b3d708bb635560a
Lau <lauraisi2000@gmail.com>
2021-10-25 12:47:07 +0200

    Update Platform.h

--- 2D platformer/Game/Source/Platform.h
+++ 2D platformer/Game/Source/Platform.h
@@ -1,6 +1,20 @@
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
 #pragma once
+
+#include "Collider.h"
 
 struct Platform
 {
+	Platform()
+	{
 
+	}
+	Collider* col;
+	Collider* down;
+	bool canFall;
+	int timeToFall;
 };
+
+#endif // !__PLATFORM_H__
